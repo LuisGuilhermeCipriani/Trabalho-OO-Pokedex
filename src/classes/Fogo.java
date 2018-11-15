98162c9a755dcfe70b41d1540fc2f51c8a1c3673
LuisGuilhermeCipriai <cipriani.luisguilherme@gmail.com>
2018-11-15 12:11:03 -0300

    Criada Classe Fogo

---
+++ src/classes/Fogo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import interfaces.IFogo;
+
+/**
+ *
+ * @author Luis Guilherme
+ */
+public class Fogo extends Pokemon implements IFogo{
+
+    public Fogo() {
+    }
+
+    public Fogo(String nome, String tipo, double peso, double altura, int ataque, int forca, int defesa, int agilidade, String ataqueEspecial, int codigo) {
+        super(nome, tipo, peso, altura, ataque, forca, defesa, agilidade, ataqueEspecial, codigo);
+    }
+    
+    
+    @Override
+    public void ataqueEspecialFogo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
