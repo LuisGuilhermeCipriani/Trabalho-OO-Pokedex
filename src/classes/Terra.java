abfd66afe3e0a843f0b3fbe2fefa36d30febd5b2
LuisGuilhermeCipriai <cipriani.luisguilherme@gmail.com>
2018-11-15 12:28:57 -0300

    Criada Classe Terra

---
+++ src/classes/Terra.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import interfaces.ITerra;
+
+/**
+ *
+ * @author Luis Guilherme
+ */
+public class Terra extends Pokemon implements ITerra{
+
+    public Terra() {
+    }
+
+    public Terra(String nome, String tipo, double peso, double altura, int ataque, int forca, int defesa, int agilidade, String ataqueEspecial, int codigo) {
+        super(nome, tipo, peso, altura, ataque, forca, defesa, agilidade, ataqueEspecial, codigo);
+    }
+
+    @Override
+    public void ataqueEspecialTerra() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
