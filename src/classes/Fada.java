1f7370b0e54d5a15df3e589626a9ec7dcadd037c
LuisGuilhermeCipriai <cipriani.luisguilherme@gmail.com>
2018-11-15 12:39:59 -0300

    Criada Classe Fada

---
+++ src/classes/Fada.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import interfaces.IFada;
+
+/**
+ *
+ * @author Luis Guilherme
+ */
+public class Fada extends Pokemon implements IFada{
+
+    public Fada() {
+    }
+
+    public Fada(String nome, String tipo, double peso, double altura, int ataque, int forca, int defesa, int agilidade, String ataqueEspecial, int codigo) {
+        super(nome, tipo, peso, altura, ataque, forca, defesa, agilidade, ataqueEspecial, codigo);
+    }
+
+    @Override
+    public void ataqueEspecialFada() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
